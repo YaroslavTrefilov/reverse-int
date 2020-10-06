0cd30ffcc8b856b52a5e8630b5f09c41af87fc7d
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-10-06 17:15:48 +0400

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n * (-1);
+  }
+  n = n.toString();
+  let reverseArr = n.split('').reverse();
+  let result = reverseArr.join('');
+
+  return +result;
 }
